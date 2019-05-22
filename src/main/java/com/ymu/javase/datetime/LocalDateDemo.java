1bb5b774b5882bd18c1604ea20bd780011d10208
xinxiamu <932852117@qq.com>
2019-05-22 19:46:24 +0800

    学习

--- src/main/java/com/ymu/javase/datetime/LocalDateDemo.java
+++ src/main/java/com/ymu/javase/datetime/LocalDateDemo.java
@@ -6,6 +6,8 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjuster;
 
+import org.junit.Test;
+
 /**
  * 人类时间。本地时间。
  * LocalDate是一个带有年份、月份、当月天数的日期。
@@ -38,5 +40,30 @@ public class LocalDateDemo {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		String s = formatter.format(ldt);
 		System.out.println(s);
+	}
+	
+	@Test
+	public void test1() {
+		LocalDate nowDate = LocalDate.now();
+		System.out.println(nowDate.getDayOfWeek().getValue());
+		System.out.println(nowDate.toString()); 
+		
+		LocalDate startDate = LocalDate.of(2019, 05, 23);
+		System.out.println(startDate.getDayOfWeek().getValue()); 
+		long diffDay = nowDate.until(startDate, ChronoUnit.DAYS);//天为表示单位，可以以其他的表示
+		System.out.println(">>>diff:" + diffDay);
+		
+		LocalDate newDate = nowDate.plusDays(20);
+		System.out.println(newDate.toString()); 
+	}
+	
+	@Test
+	public void test2() {
+		LocalDate pubD = LocalDate.of(2019, 05, 22);
+		int dw = pubD.getDayOfWeek().getValue();
+		System.out.println(dw); 
+		LocalDate enDate = pubD.plusDays(7 - dw).plusWeeks(2);
+		System.out.println(enDate);
+		System.out.println(pubD.plusWeeks(2).toString()); 
 	}
 }

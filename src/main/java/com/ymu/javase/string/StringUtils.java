12cde29c3ddb4bf5b55f8fe5b34ff12894ee14a3
xinxiamu <932852117@qq.com>
2019-06-12 10:58:16 +0800

    學習

--- src/main/java/com/ymu/javase/string/StringUtils.java
+++ src/main/java/com/ymu/javase/string/StringUtils.java
@@ -1,5 +1,34 @@
 package com.ymu.javase.string;
 
-public class StringUtils {
+import java.util.Random;
+import java.util.UUID;
+
+public final class StringUtils {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * 生成32位UUID
+	 * 
+	 * @return uid
+	 */
+	public static final String getUUID() {
+		return UUID.randomUUID().toString().replace("-", "");
+	}
+	
+	/**
+	 * 生成六位随机数
+	 */
+	public static String authCode() {
+		Random rd = new Random();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 6; i++) {
+			int num = rd.nextInt(9);
+			sb.append(num);
+		}
+		return sb.toString();
+	}
 
 }

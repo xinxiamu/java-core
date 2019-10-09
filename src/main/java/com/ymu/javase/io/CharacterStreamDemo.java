34b266d73edd4eee140e693971a9f10a6963d3cb
xinxiamu <932852117@qq.com>
2019-10-09 11:05:44 +0800

    学习

--- src/main/java/com/ymu/javase/io/CharacterStreamDemo.java
+++ src/main/java/com/ymu/javase/io/CharacterStreamDemo.java
@@ -2,15 +2,14 @@ package com.ymu.javase.io;
 
 import org.junit.Test;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * 字符流。所有字符流都继承Reader和Writer类。
  */
 public class CharacterStreamDemo {
 
+    //逐个字符读取，16位的int表示一个字符
     @Test
     public void copyCharacters() throws IOException {
         FileReader inputStream = null;
@@ -23,6 +22,30 @@ public class CharacterStreamDemo {
             int c;
             while ((c = inputStream.read()) != -1) {
                 outputStream.write(c);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+
+    //逐行读取文本
+    @Test
+    public void copyCharactersLine() throws IOException {
+        BufferedReader inputStream = null;
+        PrintWriter outputStream = null;
+
+        try {
+            inputStream = new BufferedReader(new FileReader("xanadu.txt"));
+            outputStream = new PrintWriter(new FileWriter("characteroutput.txt"));
+
+            String l; //表示一行
+            while ((l = inputStream.readLine()) != null) {
+                outputStream.write(l);
             }
         } finally {
             if (inputStream != null) {

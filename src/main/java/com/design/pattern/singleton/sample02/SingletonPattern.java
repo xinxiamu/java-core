af2b99dd725c3336d726175d312ce790b8dfe299
xinxiamu <932852117@qq.com>
2020-05-11 00:12:08 +0800

    好好学习，天天向上。

--- src/main/java/com/design/pattern/singleton/sample02/SingletonPattern.java
+++ src/main/java/com/design/pattern/singleton/sample02/SingletonPattern.java
@@ -1,15 +1,14 @@
 package com.design.pattern.singleton.sample02;
 
-//通用单例模式
+//通用单例模式，急加载
 //线程安全
 public class SingletonPattern {
 
-    private static SingletonPattern singletonPattern = new SingletonPattern();
+    private static SingletonPattern singletonPattern = new SingletonPattern(); //马上初始化，急加载
 
     private SingletonPattern() {}
 
-    //添加同步关键字，线程安全。
-    public synchronized static SingletonPattern getInstance() {
+    public static SingletonPattern getInstance() {
         return singletonPattern;
     }
 }

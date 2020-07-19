a2d91827a49d81ed3c5a3200c1ddf4c82b89edc9
xinxiamu <932852117@qq.com>
2020-07-19 10:04:11 +0800

    好好学习，天天向上。

--- src/main/java/com/ymu/javase/number/FormatNumeri.java
+++ src/main/java/com/ymu/javase/number/FormatNumeri.java
@@ -28,7 +28,7 @@ public class FormatNumeri {
 		/////////////////////
 		long n = 461012;
 	      System.out.format("%d%n", n);      //  -->  "461012"
-	      System.out.format("%08d%n", n);    //  -->  "00461012"
+	      System.out.format("%08d%n", n);    //  -->  "00461012"  //°ËÎ»£¬Ç°Ãæ²¹Áã
 	      System.out.format("%+8d%n", n);    //  -->  " +461012"
 	      System.out.format("%,8d%n", n);    // -->  " 461,012"
 	      System.out.format("%+,8d%n%n", n); //  -->  "+461,012"
